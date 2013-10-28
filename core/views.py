ab477df3733b453c439dd4192b6f410c81f12ee4
Rishi <rishi@rishi-Inspiron-N5110.(none)>
2013-10-28 20:53:19 +0530

    Rishi commit

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login as auth_login
@@ -8,7 +9,7 @@ import json
 
 def index(request,message = ''):
     if request.user.is_authenticated():
-       message = request.user.username + ', You have been registered. Session 1 is over. Session 2 starts on 27th Oct, 9:00 PM. Follow us on facebook. https://www.facebook.com/The.KGTS. You can login again under a different facebook account.'
+       message = request.user.username + ', You have been registered. Session 3 is over. Session 4 starts on 28th Oct, 9:30 PM. Follow us on facebook. https://www.facebook.com/The.KGTS. You can login again under a different facebook account.'
         #return redirect('gamnt.
     return render(request,'index.html',{'message':message})
 
